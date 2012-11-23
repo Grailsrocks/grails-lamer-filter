15d86628c9ee0a1e8c6a998904827110e9974087
Marc Palmer <marc@anyware.co.uk>
2012-11-23 15:08:01 +0000

    Updated release dep

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -31,7 +31,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         build(":tomcat:$grailsVersion",
-                ":release:2.0.2") {
+                ":release:2.1.0") {
             export = false
         }        
     }
